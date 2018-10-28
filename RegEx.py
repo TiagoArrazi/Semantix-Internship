e22856eacd56763d2f3547fb4a456717cd094d53
Tiago Arrazi <31770804+TiagoArrazi@users.noreply.github.com>
2018-10-28 19:12:07 -0300

    Update RegEx.py

--- RegEx.py
+++ RegEx.py
@@ -1,3 +1,16 @@
 import re #módulo para a utilzação de expressões regulares
+import sys #módulo para capturar parâmetros diretamente do terminal
+
+regex = re.compile('.+..:.(\w+).+:.(\w+).+.:.(\w+).+')
+
+matchString = ""
+
+with open(sys.argv[1]) as f:
+
+    for line in f:
+
+        m = regex.match(line)
+        print(m.group(1,2,3))
+
 
 
