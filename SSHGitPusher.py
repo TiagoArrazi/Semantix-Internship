69f307095c0604c23c8893e7d59649d94b561585
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-11-14 15:03:03 -0200

    Commit through GitPusher

---
+++ SSHGitPusher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#Use it by -> ./GitPusher.py (file_1) (file_2) (file_3) (file_4) ...
+#Only works with SSH
+
+import os
+import sys
+
+def getArgvSize(argv):
+
+    return len(argv)
+
+
+if __name__ == "__main__":
+
+    for i in range(1, getArgvSize(sys.argv)):
+
+        try:
+            os.system(f"git add {sys.argv[i]}")
+            os.system("git commit -m \"Commit through GitPusher\"")
+            os.system("git push -u origin master")
+
+        except IndexError:
+
+            print(" ")
+
