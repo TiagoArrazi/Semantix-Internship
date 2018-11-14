a8f9c3d6f58aef3897239b6ef645c63d371ff999
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-11-14 14:33:15 -0200

    Commit through GitPusher

--- GitPusher.py
+++ GitPusher.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 #Use it by -> ./GitPusher.py (file_1) (file_2) (file_3) (file_4) ...
-#Only works with SSH
+#Onlly works with SSH
 
 import os
 import sys
-import time
 
 def getArgvSize(argv):
 
@@ -14,9 +13,13 @@ def getArgvSize(argv):
 
 if __name__ == "__main__":
 
-    for i in range(1, getArgvSize(sys.argv)):
+    for i in range(1, getArgvSize(sys.argv) + 1):
 
-        os.system(f"git add {sys.argv[i]}")
-        os.system("git status")
-        os.system("git commit -m \"Commit through GitPusher\"")
-        os.system("git push -u origin master")
+        try:
+            os.system(f"git add {sys.argv[i]}")
+            os.system("git commit -m \"Commit through GitPusher\"")
+            os.system("git commit -u origin master")
+
+        except IndexError:
+
+            print("Out of bounds")
