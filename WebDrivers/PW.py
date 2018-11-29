cc06aad58f591ef9ddd696ff96985dbcaf482a31
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-11-29 13:53:01 -0200

    Commit through GitPusher

---
+++ WebDrivers/PW.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from math import pi, e, tau
+
+def getAP_631():
+    
+    return str(pi).replace('.','')
+
+def getAP2():
+
+    return str(e).replace('.','')
+
+def getAP3():
+
+    return str(tau).replace('.','')
+
+
+
