8d99abe5f1682767ba7dcf7a07721d477a070ce0
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-11-29 13:08:06 -0200

    Commit through GitPusher

--- WebDriver.py
+++ WebDriver.py
@@ -1,37 +1,43 @@
 #! /usr/bin/env python3
 
 from selenium import webdriver
-from selenium.webdriver.common import action_chains, keys
-from time import sleep
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from PW import getAP_631
+
+delay = 10
+identity = "automatontest631@gmail.com"
+comment = "This comment was sent by an automation software made with Selenium and Python"
+url = "https://www.youtube.com/watch?v=8ZtInClXe1Q&t=1s"
 
 driver = webdriver.Chrome()
-action = action_chains.ActionChains(driver)
 driver.maximize_window()
 
-driver.get("https://www.youtube.com/watch?v=8ZtInClXe1Q&t=1s")
+driver.get(url)
 
-sleep(2)
+signInButton = WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//ytd-button-renderer[@class='style-scope ytd-masthead style-blue-text size-default']")))
+signInButton.click()
 
-driver.find_element_by_xpath("//ytd-button-renderer[@class='style-scope ytd-masthead style-blue-text size-default']").click()#Sign in button
+idTextField = WebDriverWait(driver,delay).until(EC.visibility_of_element_located((By.XPATH, "//input[@type='email']")))
+idTextField.send_keys(identity)
+idTextField.send_keys(keys.Keys.ENTER)
 
-driver.find_element_by_xpath("//input[@type='email']").send_keys("tiago.costa@semantix.com.br")
-driver.find_element_by_id("identifierNext").click()
+pwTextField = WebDriverWait(driver,delay).until(EC.visibility_of_element_located((By.XPATH, "//input[@type='password']")))
+pwTextField.send_keys(getAP_631())
+pwTextField.send_keys(keys.Keys.ENTER)
 
-sleep(1)
+YTLikeButton = WebDriverWait(driver,delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='top-level-buttons']/ytd-toggle-button-renderer[1]")))
+YTLikeButton.click()
 
-driver.find_element_by_xpath("//input[@type='password']").send_keys("morrados2018!@#")
-driver.find_element_by_id("passwordNext").click()
-
-sleep(5)
-
-driver.find_element_by_xpath("//*[@id='top-level-buttons']/ytd-toggle-button-renderer[1]").click()
 driver.execute_script("window.scrollTo(0, 450)") 
 
-sleep(3)
+YTUnclickedCommentTextField = WebDriverWait(driver,delay).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='placeholder-area']")))
+YTUnclickedCommentTextField.click()
 
-driver.find_element_by_xpath("//*[@id='placeholder-area']").click()
-driver.find_element_by_xpath("//*[@id='contenteditable-textarea']").send_keys("This comment was sent by an automation software made with Selenium and Python")
+YTClickedCommentTextField = driver.find_element_by_xpath("//*[@id='contenteditable-textarea']")
+YTClickedCommentTextField.send_keys(comment)
 
-sleep(3)
-
-driver.find_element_by_xpath("//*[@id='submit-button']").click()
+commentButton = WebDriverWait(driver,delay).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='submit-button']")))
+commentButton.click()
