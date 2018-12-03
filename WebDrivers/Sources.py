aea9057711d2d73df7e69d918f75a4f9a55d8e99
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-12-03 08:53:52 -0200

    Commit through GitPusher

--- WebDrivers/Sources.py
+++ WebDrivers/Sources.py
@@ -2,12 +2,6 @@
 
 from math import pi, e, tau
 
-def getXPath(i):
-
-    xpathList = ["//ytd-button-renderer[@class='style-scope ytd-masthead style-blue-text size-default']", "//input[@type='email']", "//input[@type='password']", "//*[@id='top-level-buttons']/ytd-toggle-button-renderer[1]", "//*[@id='placeholder-area']", "//*[@id='contenteditable-textarea']", "//*[@id='submit-button']"]
-
-    return xpathList[i]
-
 def getAP_631():
     
     return str(pi).replace('.','')
