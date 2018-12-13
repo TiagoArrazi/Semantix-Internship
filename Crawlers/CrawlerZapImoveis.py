5548d3603dce6eea350d8aee4a994b6f3b17a26a
Tiago Arrazi <tiago_arrazi98@outlook.com>
2018-12-13 14:16:45 -0200

    Commit through GitPusher

--- Crawlers/CrawlerZapImoveis.py
+++ Crawlers/CrawlerZapImoveis.py
@@ -1,30 +1,66 @@
 #! /usr/bin/env python3
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from bs4 import BeautifulSoup
-from requests import get 
+from requests import get
 from csv import writer
 from os import stat
 from datetime import datetime
 
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.add_argument("--headless")
+OPTIONS.add_argument("""--user-agent=Mozilla/5.0 
+                                     (X11; Linux x86_64) 
+                                     AppleWebKit/537.36 
+                                     (KHTML, like Gecko) 
+                                     Chrome/47.0.2526.80 
+                                     Safari/537.36""")
+
+driver = webdriver.Chrome(chrome_options=OPTIONS)
+
 for i in range(1, 11):
 
-    url = ("https://www.zapimoveis.com.br/" +
-          "aluguel/imoveis/" + 
-          "#{\"pagina\":\"" + str(i) + "\",\"paginaOrigem\":\"Home\",\"formato\":\"Galeria\"}")
+    URL = "https://www.zapimoveis.com.br/aluguel/imoveis/#{%22pagina%22:" 
+           + str(i) 
+           + ",%22paginaOrigem%22:%22Home%22,%22formato%22:%22Galeria%22}"
 
-    requestString = get(url=url, headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"})
+    HEADERS = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
 
-    soup = BeautifulSoup(requestString.text, "html.parser")
+    driver.get(URL)
+    response = get(url=driver.current_url, headers=HEADERS).text
+
+    soup = BeautifulSoup(response, "html.parser")
     realties = soup.find_all("article", {"class":"minificha"}) #list of HTML-shaped realties info
     updates = soup.find_all("em", {"class":"atualizacao"}) #list of HTML-shaped info updates
 
     for k in range(0,len(realties)):
 
         try:
+
             realties[k] = eval(realties[k]['data-clickstream']) #turning 'data-clickstream' dictionary into an actual python dictionary
+            key_list = list(realties[k].keys())
+
+            for key in key_list:
+
+                if isinstance(realties[k][key], list):
+                    if len(realties[k][key]) == 0:
+                        realties[k][key] = '0'
+
         except NameError:
+
             realties[k] = eval(realties[k]['data-clickstream']) #fix for an unknown bug 
-            print("Error")
+            key_list = list(realties[k].keys())
+
+            for key in key_list:
+
+                if isinstance(realties[k][key], list):
+                    if len(realties[k][key]) == 0:
+                        realties[k][key] = '0'
 
     for l in range(0,len(updates)):
         updates[l] = updates[l].get_text() #extracting text from updates
@@ -75,16 +111,16 @@ for i in range(1, 11):
 
         date = datetime.strptime((requestString.headers["Date"][:-4]), "%a, %d %b %Y %H:%M:%S")
 
-        if realties[j]['unitTypes'] == "Studio": #if the realty is a Studio, it will write in both Residential and Business files
+        if realties[j]['unitTypes'][0] == "Studio": #if the realty is a Studio, it will write in both Residential and Business files
 
-            res_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'], 
-                            realties[j]['areas'], realties[j]['bedrooms'], realties[j]['suites'], realties[j]['bathrooms'],
-                            realties[j]['parkingSpaces'], realties[j]['rentalPrices'], realties[j]['condoFee'], realties[j]['iptuPrices'], 
+            res_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'][0], 
+                            realties[j]['areas'][0], realties[j]['bedrooms'][0], realties[j]['suites'][0], realties[j]['bathrooms'][0],
+                            realties[j]['parkingSpaces'][0], realties[j]['rentalPrices'], realties[j]['condoFee'], realties[j]['iptuPrices'], 
                             realties[j]['address'][0],realties[j]['address'][1], realties[j]['address'][2],
                             realties[j]['address'][3], realties[j]['address'][4], realties[j]['address'][5],
                             date, updates[j]])
 
-            com_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'], 
+            com_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'][0], 
                             realties[j]['areas'], realties[j]['bedrooms'], realties[j]['suites'], realties[j]['bathrooms'], 
                             realties[j]['parkingSpaces'], realties[j]['rentalPrices'], realties[j]['condoFee'], realties[j]['iptuPrices'], 
                             realties[j]['address'][0],realties[j]['address'][1], realties[j]['address'][2], 
@@ -97,9 +133,9 @@ for i in range(1, 11):
             continue
 
 
-        elif realties[j]['unitTypes'] in unitTypes_res: #if the realty type is in the residential realties list, 
+        elif realties[j]['unitTypes'][0] in unitTypes_res: #if the realty type is in the residential realties list, 
                                          #redirects the writerow() to the Residential file
-            res_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'], 
+            res_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'][0], 
                             realties[j]['areas'], realties[j]['bedrooms'], realties[j]['suites'], realties[j]['bathrooms'], 
                             realties[j]['parkingSpaces'], realties[j]['rentalPrices'], realties[j]['condoFee'], realties[j]['iptuPrices'], 
                             realties[j]['address'][0],realties[j]['address'][1], realties[j]['address'][2], 
@@ -109,7 +145,7 @@ for i in range(1, 11):
             res_csv.flush()
 
 
-        elif realties[j]['unitTypes'] in unitTypes_com: #if the realty type is in the business realties list, 
+        elif realties[j]['unitTypes'][0] in unitTypes_com: #if the realty type is in the business realties list, 
                                          #redirects the writerow() to the Business file
             com_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'], 
                             realties[j]['areas'], realties[j]['bedrooms'], realties[j]['suites'], realties[j]['bathrooms'], 
@@ -121,7 +157,7 @@ for i in range(1, 11):
             com_csv.flush()
 
 
-        elif realties[j]['unitTypes'] in unitTypes_rur: #if the realty type is in the countryside realties list,
+        elif realties[j]['unitTypes'][0] in unitTypes_rur: #if the realty type is in the countryside realties list,
                                          #redirects the writerow() to the Countryside file
             rur_w.writerow([realties[j]['listingId'], realties[j]['unitTypes'], 
                             realties[j]['areas'], realties[j]['bedrooms'], realties[j]['suites'], realties[j]['bathrooms'], 
@@ -132,9 +168,11 @@ for i in range(1, 11):
 
             rur_csv.flush()
             
-        res_csv.close()
-        com_csv.close()
-        rur_csv.close()
+    res_csv.close()
+    com_csv.close()
+    rur_csv.close()
+
+    driver.execute_script("document.getElementById('proximaPagina').click()")
 
 
 
