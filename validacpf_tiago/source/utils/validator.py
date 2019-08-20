f420ae5aae3c9bae0c6dc86843569e965257d1ec
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-08-20 08:32:46 -0300

    Correção na validação do CPF

--- validacpf_tiago/source/utils/validator.py
+++ validacpf_tiago/source/utils/validator.py
@@ -3,9 +3,13 @@ class Validator:
     @classmethod
     def validate(cls, input_string):
         first_digit_weight_dict = dict()
+
+        if [input_string[3], input_string[7], input_string[11]] != ['.', '.', '-']:
+            return False
+
         formatted_string = cls.format_(input_string)
 
-        if len(set(list(formatted_string))) == 1:
+        if len(set(list(formatted_string))) == 1 or len(formatted_string) != 11:
             return False
 
         [first_digit_weight_dict.update({each: index}) for index, each in zip(range(10, 1, -1), formatted_string[:-2])]
--- validacpf_tiago/test/test_classes.py
+++ validacpf_tiago/test/test_classes.py
@@ -5,11 +5,12 @@ from source.utils.validator import Validator
 class Tester(unittest.TestCase):
 
     def test_format_(self):
-        self.assertEqual(Validator.format_('123-456-789-09'), '12345678909')
+        self.assertEqual(Validator.format_('123.456.789-09'), '12345678909')
 
     def test_validate(self):
-        self.assertTrue(Validator.validate('123-456-789-09'))
-        self.assertFalse(Validator.validate('123-456-789-10'))
+        self.assertTrue(Validator.validate('123.456.789-09'))
+        self.assertFalse(Validator.validate('123.456.789-10'))
+        self.assertFalse(Validator.validate('1234.56.789-10'))
         self.assertFalse(Validator.validate('111.111.111-11'))
 
 
